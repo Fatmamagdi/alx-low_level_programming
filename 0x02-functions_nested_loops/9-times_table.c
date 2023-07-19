c8cec533dd7e1b2ccabf852f2d089991cdc59870
fatmamagdi <fatmamagdi033@gmail.com>
2023-07-19 16:31:03 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -30,12 +30,9 @@ void times_table(void)
 			if (prod <= 9)
 				_putchar(' ');
 			else
-
 				_putchar((prod / 10) + 48); /*get the first digit*/
-
 			_putchar((prod % 10) + 48); /*get the second digit*/
-			
 		}
-		_putchar('\n');
+	_putchar('\n');
 	}
 }
