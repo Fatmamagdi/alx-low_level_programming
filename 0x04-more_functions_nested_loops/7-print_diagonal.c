135e7cdd61d8e68919c6e14326bad60047f4fc88
fatmamagdi <fatmamagdi033@gmail.com>
2023-07-28 16:52:14 -0700

    low

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,22 +7,23 @@
  */
 void print_diagonal(int n)
 {
-    if (n <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
 
-    int spaces;
-    for (int i = 0; i < n; i++)
-    {
-        spaces = i;
-        while (spaces > 0)
-        {
-            _putchar(' ');
-            spaces--;
-        }
-        _putchar('\\');
-        _putchar('\n');
-    }
+	int spaces;
+
+	for (int i = 0; i < n; i++)
+	{
+		spaces = i;
+		while (spaces > 0)
+		{
+			_putchar(' ');
+			spaces--;
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
 }
