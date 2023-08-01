704811aecf530cbb77f90d1510308132281aed83
fatmamagdi <fatmamagdi033@gmail.com>
2023-08-01 08:56:34 -0700

    0x07

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+char *_memset(char *s, char b, unsigned int n)
+{
+        int x;
+        /* Number of chracters will change */
+        for (x = 0; x < n; x++)
+        {
+                /* change the chracter with b */
+                s[x] = b;
+        }
+        return (s);
+}
